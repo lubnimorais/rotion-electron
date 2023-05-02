00ae2c5abd37b64a78d64a9771b1ed0c414943d8
Lubni Morais <lubni.morais@gmail.com>
2023-05-01 21:27:43 -0300

    feat: Esqueleto da aplicação

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,21 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import './styles/globa.css';
 
 const App = () => {
-  return <h1 className="text-violet-400 font-bold text-4xl">Hello World</h1>;
+  return (
+    <div className="w-screen h-screen text-rotion-100 flex">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col max-h-screen">
+        <Header />
+
+        <main className="flex flex-1 items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export { App };
